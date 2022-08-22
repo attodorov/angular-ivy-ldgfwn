2a75ef121e7d73a1f092cc4c195eaa7771fbee5e
attodorov <attodorov@gmail.com>
2022-08-23 00:03:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ldgfwn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ldgfwn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name: string = 'Angular ' + VERSION.major;
+  firstname: string = '';
+  lastname: string = '';
+  constructor() {}
+  ngOnInit() {
+    this.name +=
+      '. Entered names:  ' +
+      this.firstname +
+      ' ' +
+      this.lastname;
+  }
+}
